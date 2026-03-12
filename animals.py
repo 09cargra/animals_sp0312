23255fbd297ffd74786c27560112f51d21bda046
09cargra <09cargra@skola.boras.se>
2026-03-12 13:58:49 +0100

    added cow sound

--- animals.py
+++ animals.py
@@ -2,12 +2,15 @@ def main():
     while True:
         ## Skriv ut menyvalen
         print("1. Cat")
+        print("2. Cow")
         print("Enter x to exit")
 
         choice = input("Enter your selection")
         match choice.lower():
             case '1': #Cat-sound
                 print("Mjau, mjau")
+            case '2': #Cow-sound
+                print("Muu, muu")
             case 'x': #Exit
                 break
             case _:
